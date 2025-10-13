f8bfaeebc413327402c5cd01b3fa5dfdc781f35c
Francesco Trematerra <fvtae1@iscte-iul.pt>
2025-10-13 22:44:35 +0100

    Alterei o pom e criei 2 funções para a geração do qr code

---
+++ src/main/java/com/example/qrcode/QRCodeGenerator.java
@@ -0,0 +1,25 @@
+package com.example.qrcode;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class QRCodeGenerator {
+
+    public static String generateBase64QRCode(String text) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+
+        return "data:image/png;base64," + Base64.getEncoder().encodeToString(pngData);
+    }
+}
